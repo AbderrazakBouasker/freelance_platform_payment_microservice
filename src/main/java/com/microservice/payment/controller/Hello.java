a8633ce1eb848dcad7efb6ec59c59c3787488571
Jose Mokeni <jmmokeni@gmail.com>
2024-10-28 22:36:27 +0100

    change hello route

--- src/main/java/com/microservice/payment/controller/Hello.java
+++ src/main/java/com/microservice/payment/controller/Hello.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-    @GetMapping("/payment/hello")
+    @GetMapping("/hello")
     String sayHello() {
-        return "Hello World";
+        return "Hello World from Payment Microservice";
     }
 
 }
